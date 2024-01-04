c7fba54194d341c760528cb9e7a18c9fd446f6f7
saad habibi <syedsaadhabibi@gmail.com>
2024-01-04 17:44:03 +0530

    Initial commit

---
+++ apipro.js
@@ -0,0 +1,31 @@
+'X-RapidAPI-Key': '5145088e44msh3830fbcaa8f08e5p1b7a9djsn3a8d3986c502',
+'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+
+
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    if (location) {
+        fetchWeather(location);
+    }
+});
+
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
